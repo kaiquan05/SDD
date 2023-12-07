ae364a6944bf567514282c51119fda517582b9b7
burntnuggets <livialim2237@gmail.com>
2023-12-08 01:55:28 +0800

    partial documentation

--- main.py
+++ main.py
@@ -36,8 +36,7 @@ pointsRules = {
 # draw_field()
 #
 #    Draws the field of play
-#    The column numbers only go to 3 since players can only place units
-#      in the first 3 columns
+#
 #----------------------------------------------------------------------
 field = [ [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],
           [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],
@@ -85,11 +84,13 @@ def draw_field():
         print('|')
         print(" +----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+")
 
+# get building function to get two random buildings for the user to select
 def getRandomBuildings():
     r1 = random(list(buildingList))
     r2 = random(list(buildingList))
     return [r1,r2]
 
+# validation function to check if user has entered a valid choice 
 def getOptions(i, lower, upper):
     choice = input(i)
     if not choice.isdigit():
@@ -100,6 +101,7 @@ def getOptions(i, lower, upper):
     return False
 
 def gameTurn():
+    # game menu
     if State['Turn'] > len[field] * len[field][0]:
         return True
     draw_field()    
@@ -111,33 +113,35 @@ def gameTurn():
     print('[4] Exit to Main Menu')
 
     while True:
+        # validation to checek that user has entered a valid choice
         choice = getOptions("Your Choice: ", 1, 4)
         if choice is not False:
             break
         print ("Invalid choice. Please choose again")
     
     match choice:
-        case 1:
+        case 1: # build building
             bList = getRandomBuildings()
             print(f'Building 1: {bList[0]}, Building 2: {bList[1]}')
             ok = gameBuild("Your Choice: ")
-        case 2:
+        case 2: # view current score
             print(State['Points'])
-        case 3:
+        case 3: # save game
             print("Save gameeeeee")
-        case 4:
+        case 4: # exit to main menu
             return True
 
+# build building function
 def gameBuild(b):
-    coords = input("Build where? ")
+    coords = input("Build where? ") # user input for building location
     x = coords[0]
-    if not x.isalpha():
+    if not x.isalpha(): # validation to ensure that a valid column is inputted
         return False
     x = ord(x.lower) - 96
     y = coords[1]
-    if (x <= 20 and y <= 20):
+    if (x <= 20 and y <= 20):  # validation to ensure that a valid row is inputted
         print("")
-    if State['Turn'] is 1:
+    if State['Turn'] is 1: # first turn
         field[x][y] = b
 
 #Main program
@@ -150,22 +154,23 @@ while exitMainMenu == False:
     print('[4] Exit Game')
 
     while True:
+        # validation to checek that user has entered a valid choice
         choice = getOptions("Your Choice: ", 1, 4)
         if choice is not False:
             break
         print ("Invalid choice. Please choose again")
 
-    match choice:
-        case 1: 
+    match choice: # associate choice to respective game features
+        case 1:  # start new game
             draw_field()
             gameFinish = gameTurn()
             if gameFinish:
                 break
-        case 2:
+        case 2: # load a previously created game
             "Load new game"
-        case 3:
+        case 3: # display all high scores
             "Display high score"
-        case 4:
+        case 4: # exit the game into main menu
             exitMainMenu = True
 
 
