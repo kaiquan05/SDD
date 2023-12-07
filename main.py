314147e02e66ff1ccb21d71aab79f236f5b6ae18
Kai Quan <S10241963@connect.np.edu.sg>
2023-12-08 00:41:58 +0800

    half gameBuild

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ import random
 exitMainMenu = False
 
 buildingList = {'R': "Residential", 'I': "Industry", 'C': "Commercial", 'O': "Park", '*': "Road"}
-defaultCoin = 16
-Turn = 1
-scoring_rules = {
+State = {
+    "Turn" : 1,
+    "Points" : 0,
+    "Coins": 16
+}
+pointsRules = {
     "Residential": {
         "Industry": 1,
         "Residential": 1,
@@ -97,9 +100,46 @@ def getOptions(i, lower, upper):
     return False
 
 def gameTurn():
-    if Turn > len[field] * len[field][0]:
+    if State['Turn'] > len[field] * len[field][0]:
         return True
+    draw_field()    
+    print('')
+    print('----------------------- Ngee Ann City-----------------------')
+    print('[1] Build a Building')
+    print('[2] See Current Score')
+    print('[3] Save Game')
+    print('[4] Exit to Main Menu')
+
+    while True:
+        choice = getOptions("Your Choice: ", 1, 4)
+        if choice is not False:
+            break
+        print ("Invalid choice. Please choose again")
     
+    match choice:
+        case 1:
+            bList = getRandomBuildings()
+            print(f'Building 1: {bList[0]}, Building 2: {bList[1]}')
+            ok = gameBuild("Your Choice: ")
+        case 2:
+            print(State['Points'])
+        case 3:
+            print("Save gameeeeee")
+        case 4:
+            return True
+
+def gameBuild(b):
+    coords = input("Build where? ")
+    x = coords[0]
+    if not x.isalpha():
+        return False
+    x = ord(x.lower) - 96
+    y = coords[1]
+    if (x <= 20 and y <= 20):
+        print("")
+    if State['Turn'] is 1:
+        field[x][y] = b
+
 #Main program
 while exitMainMenu == False:
     print('')
@@ -113,18 +153,14 @@ while exitMainMenu == False:
         choice = getOptions("Your Choice: ", 1, 4)
         if choice is not False:
             break
-        print('')
-        print('------------------Welcome to Ngee Ann City------------------')
-        print('[1] Start New Game')
-        print('[2] Load Saved Game')
-        print('[3] Display High Scores')
-        print('[4] Exit Game')
         print ("Invalid choice. Please choose again")
 
     match choice:
         case 1: 
             draw_field()
-            
+            gameFinish = gameTurn()
+            if gameFinish:
+                break
         case 2:
             "Load new game"
         case 3:
